6a145664693d9574258cb7cf4fe5318394b42feb
eamiear <2639121731@qq.com>
2018-07-31 23:00:50 +0800

    [ura-common] Query

--- ura-common/src/main/java/com/ura/common/utils/Query.java
+++ ura-common/src/main/java/com/ura/common/utils/Query.java
@@ -5,6 +5,7 @@
 
 package com.ura.common.utils;
 
+import com.baomidou.mybatisplus.plugins.Page;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.LinkedHashMap;
@@ -12,23 +13,29 @@ import java.util.Map;
 
 public class Query extends LinkedHashMap<String, Object>{
 
+    private Page<?> mybatisPlusPage;
     // 页码
-    private int page;
+    private int page = 1;
     // 记录数/每页
-    private int limit;
+    private int limit = 10;
 
     public Query(Map<String, Object> params) {
         this.putAll(params);
 
         Object _page = params.get("page");
         Object _limit = params.get("limit");
-        if (_page != null && _limit != null) {
+        if (_page != null) {
             this.page = Integer.parseInt(_page.toString());
-            this.limit = Integer.parseInt(_limit.toString());
-            this.put("offset", (page - 1) * limit);
-            this.put("page", page);
-            this.put("limit", limit);
         }
+        if (_limit != null) {
+            this.limit = Integer.parseInt(_limit.toString());
+        }
+
+        this.put("offset", (page - 1) * limit);
+        this.put("page", page);
+        this.put("limit", limit);
+
+        this.mybatisPlusPage = new Page<>(page, limit);
     }
 
     public int getPage() {

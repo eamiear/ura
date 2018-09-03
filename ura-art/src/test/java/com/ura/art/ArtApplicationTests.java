0d6c8db4eb975cfc6bfc97b6d35a368c3474540b
eamiear <2639121731@qq.com>
2018-09-04 00:23:07 +0800

    [ura-art]

---
+++ ura-art/src/test/java/com/ura/art/ArtApplicationTests.java
@@ -0,0 +1,31 @@
+package com.ura.art;
+
+import com.ura.art.config.DrawerUtils;
+import com.ura.art.config.FontUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.awt.*;
+import java.io.File;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void getWord() {
+		Font font = FontUtils.getFont("CTFB");
+		File file = new File("E:\\userCTFB.png");
+		try {
+			DrawerUtils.createImage("姜育恒", font, file, 100, 100);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}

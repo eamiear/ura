538e6557a22efa5b1766db5b07516200d9ce5039
eamiear <2639121731@qq.com>
2018-09-15 23:44:44 +0800

    [ura-api]

---
+++ ura-api/src/main/java/com/ura/api/interceptor/AuthorizationInterceptor.java
@@ -0,0 +1,12 @@
+package com.ura.api.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * Token验证
+ */
+@Component
+public class AuthorizationInterceptor extends HandlerInterceptorAdapter{
+
+}

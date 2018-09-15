7b7d13422802b97dcc8f8e7b745aff1aba66599f
eamiear <2639121731@qq.com>
2018-09-16 01:36:47 +0800

    [ura-api]

--- ura-api/src/main/java/com/ura/api/service/UserService.java
+++ ura-api/src/main/java/com/ura/api/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ura.api.service;
 
 import com.baomidou.mybatisplus.service.IService;
+import com.ura.api.bean.LoginBean;
 import com.ura.common.utils.PageUtils;
 import com.ura.api.entity.UserEntity;
 import java.util.Map;
@@ -11,4 +12,9 @@ import java.util.Map;
  * @datetime 2018-09-12 17:46:35
  */
 public interface UserService extends IService<UserEntity> {
+    UserEntity queryByMobile(String mobile);
+
+    UserEntity queryByOpenId(String openId);
+
+    Map<String, Object> login(LoginBean login);
 }
--- ura-api/src/main/java/com/ura/api/service/impl/UserServiceImpl.java
+++ ura-api/src/main/java/com/ura/api/service/impl/UserServiceImpl.java
@@ -1,25 +1,52 @@
 package com.ura.api.service.impl;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
-import com.ura.common.utils.PageUtils;
-import com.ura.common.utils.Query;
+import com.ura.api.bean.LoginBean;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import com.ura.api.dao.UserDao;
+import com.ura.api.entity.TokenEntity;
 import com.ura.api.entity.UserEntity;
+import com.ura.api.service.TokenService;
 import com.ura.api.service.UserService;
+import com.ura.common.exception.URAException;
+import com.ura.common.validator.Assert;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("tbUserService")
 public class UserServiceImpl extends ServiceImpl<UserDao, UserEntity> implements UserService {
-    @Override
-    public PageUtils queryPage(Map<String, Object> params) {
-        Page<UserEntity> page = this.selectPage(
-                new Query<UserEntity>(params).getPage(),
-                new EntityWrapper<UserEntity>());
-        return new PageUtils(page);
+  @Autowired
+  private TokenService tokenService;
+
+  @Override
+  public UserEntity queryByMobile(String mobile) {
+    UserEntity user = new UserEntity();
+    user.setMobile(mobile);
+    return baseMapper.selectOne(user);
+  }
+
+  @Override
+  public UserEntity queryByOpenId(String openId) {
+    return this.selectOne(new EntityWrapper<UserEntity>().eq("weixin_openid", openId));
+  }
+
+  @Override
+  public Map<String, Object> login(LoginBean login) {
+    UserEntity user = queryByMobile(login.getMobile());
+    Assert.isNull(user, "手机号或密码错误");
+
+    if (!user.getPassword().equals(DigestUtils.sha256Hex(login.getPassword()))){
+      throw new URAException("手机号或密码错误");
     }
+    TokenEntity token = tokenService.createToken(user.getUserId());
+    Map<String, Object> map = new HashMap<>(2);
+    map.put("token", token.getToken());
+    map.put("expire", token.getExpireTime().getTime() - System.currentTimeMillis());
+    return map;
+  }
 }

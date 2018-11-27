0d17744c06177630aae8cc6431f6b3fc74b62865
eamiear <2639121731@qq.com>
2018-11-28 00:08:12 +0800

    [ura-ai]

--- ura-ai/src/main/java/com/ura/ai/AiApplication.java
+++ ura-ai/src/main/java/com/ura/ai/AiApplication.java
@@ -3,7 +3,7 @@ package com.ura.ai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ura.core", "com.ura.ai"})
 public class AiApplication {
 
 	public static void main(String[] args) {
